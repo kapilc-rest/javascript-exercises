5fa535b0f0d86ef5f780bb4b9daf24dfaf7b2f4d
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-20 04:10:42 +0000

    Make the function

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+        if(num < 0)
+            return "Error";
+        else { 
+            let repeat = "";
+            for (let i = 0; i < num; i++) {
+                repeat += string;
+        }
+        return repeat;
+    };
 };
 
 // Do not edit below this line
