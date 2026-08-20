9904ba5b76f0971dca29efd21e544d688b2d9401
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-20 04:36:31 +0000

    Add concise comment

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
 const reverseString = function(str) {
     return str.split(" ").reverse().map((word) => word.split("").reverse().join("")).join(" ");
+
+    //easier: return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
