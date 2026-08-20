d3a9fad8dbbe163d713d5ff6c74fae596bf4074e
kapilc-rest <311136214+kapilc-rest@users.noreply.github.com>
2026-08-20 06:14:29 +0000

    Make the function and run through all test

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if (Number.isInteger(a) && Number.isInteger(b) && a >0 && b>0) {
+        let arr = [a,b].sort((a,b) => a-b);
+        let sum = 0;
+        for(let i = arr[0]; i <= arr[1] ; i++)
+            {
+                sum += i;
+            } 
+        return sum;
+    }
+    else return "ERROR";
 };
 
 // Do not edit below this line
